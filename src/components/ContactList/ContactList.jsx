9d64a5bb4912911bc92e0c0f2f070b363dfd92d1
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-05-01 00:38:23 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,7 +4,7 @@ import css from './ContactList.module.css';
 const ContactList = ({ contacts, onDelete }) => {
   return (
     <ul className={css.contact__list}>
-      {contacts.map(({ id, name, number }, idx) => (
+      {contacts.map(({ id, name, number }) => (
         <li key={id} className={css.contact__item}>
           <div>
             <p className={css.contact__list__text}>{name}</p>
@@ -25,6 +25,12 @@ const ContactList = ({ contacts, onDelete }) => {
 export default ContactList;
 
 ContactList.propeTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.object),
-  onDelete: PropTypes.func,
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
