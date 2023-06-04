7ca2f34d05eb424697f56a9ab5657384e6686464
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-06-04 16:40:54 +0300

    Add toastify and debounce filter

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import css from './ContactForrm.module.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts } from 'redux/selectors';
 import { addContact } from 'redux/contactsSlice';
@@ -18,9 +20,11 @@ const ContactForm = () => {
     );
 
     if (equalName)
-      return alert(`This name ${equalName.name} is already in contacts.`);
+      return toast.warning(
+        `This name ${equalName.name} is already in contacts.`
+      );
     if (equalNumber)
-      return alert(
+      return toast.warning(
         `This number ${equalNumber.number} is already in contacts and belongs to ${equalNumber.name}.`
       );
 
@@ -33,37 +37,53 @@ const ContactForm = () => {
   };
 
   return (
-    <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
-      <Form>
-        <label className={css.contact__label}>
-          Name
-          <Field
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            className={css.contact__input}
-          />
-          <ErrorMessage name="name" component="div" />
-        </label>
-        <label className={css.contact__label}>
-          Number
-          <Field
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            className={css.contact__input}
-          />
-        </label>
-        <ErrorMessage name="number" component="div" />
-        <button type="submit" className={css.contact__button}>
-          Add contact
-        </button>
-      </Form>
-    </Formik>
+    <>
+      <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
+        <Form>
+          <label className={css.contact__label}>
+            Name
+            <Field
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              className={css.contact__input}
+            />
+            <ErrorMessage name="name" component="div" />
+          </label>
+          <label className={css.contact__label}>
+            Number
+            <Field
+              type="tel"
+              name="number"
+              placeholder="Enter number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              className={css.contact__input}
+            />
+          </label>
+          <ErrorMessage name="number" component="div" />
+          <button type="submit" className={css.contact__button}>
+            Add contact
+          </button>
+        </Form>
+      </Formik>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 };
 
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import css from './Filter.module.css';
 import { useDispatch } from 'react-redux';
 import { setFilter } from 'redux/filtersSlice';
+import debounce from 'lodash.debounce';
 
 const Filter = () => {
   const dispatch = useDispatch();
@@ -11,10 +12,18 @@ const Filter = () => {
     dispatch(setFilter(value));
   };
 
+  const delayedOnChange = debounce(onChange, 300);
+
   return (
     <label className={css.filter__label}>
       Find contact by name
-      <input type="text" className={css.filter__input} onChange={onChange} />
+      <input
+        type="text"
+        name="filter"
+        placeholder="Enter name for Search"
+        className={css.filter__input}
+        onChange={delayedOnChange}
+      />
     </label>
   );
 };
