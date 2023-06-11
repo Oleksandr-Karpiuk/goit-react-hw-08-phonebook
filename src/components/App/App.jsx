37b3b8672e69e585d7ce980307662525d2c3a3db
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-06-11 15:43:55 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,11 +7,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
 import { selectContacts, selectError, selectIsLoading } from 'redux/selectors';
 import { Dna } from 'react-loader-spinner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const contacts = useSelector(selectContacts);
   const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  const isError = useSelector(selectError);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,40 +21,54 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className={css.app}>
-      {error && <h2 className={css.secondary__title}>{error}</h2>}
-      <h1 className={css.main__title}>Phonebook</h1>
-      <ContactForm />
-      {contacts.length ? (
-        <div>
-          <h2 className={css.secondary__title}>Contacts</h2>
-          <>
-            {isLoading ? (
-              <Dna
-                visible={true}
-                height="200"
-                width="200"
-                ariaLabel="dna-loading"
-                wrapperStyle={{
-                  position: 'fixed',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                }}
-                wrapperClass="dna-wrapper"
-              />
-            ) : (
-              <>
-                <Filter />
-                <ContactList />
-              </>
-            )}
-          </>
-        </div>
-      ) : (
-        <h2 className={css.secondary__title}>No any contacts</h2>
-      )}
-    </div>
+    <>
+      <div className={css.app}>
+        {isError && toast.error(isError)}
+        <h1 className={css.main__title}>Phonebook</h1>
+        <ContactForm />
+        {contacts.length ? (
+          <div>
+            <h2 className={css.secondary__title}>Contacts</h2>
+            <>
+              {isLoading ? (
+                <Dna
+                  visible={true}
+                  height="200"
+                  width="200"
+                  ariaLabel="dna-loading"
+                  wrapperStyle={{
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                  }}
+                  wrapperClass="dna-wrapper"
+                />
+              ) : (
+                <>
+                  <Filter />
+                  <ContactList />
+                </>
+              )}
+            </>
+          </div>
+        ) : (
+          <h2 className={css.secondary__title}>No any contacts</h2>
+        )}
+      </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 };
 
